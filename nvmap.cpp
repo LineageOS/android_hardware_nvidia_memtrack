805312610d41e48cb80ae0b6b310dea90e4cd735
Aaron Kling <webgeek1234@gmail.com>
2023-12-16 15:01:02 -0600

    Fix up nvmap memory parsing
    Upstream hal does not track secure, but does track dedicated versus
    system.
    
    Change-Id: Ib2902469423a699e44e2d14bbbb265457e1cef89

--- nvmap.cpp
+++ nvmap.cpp
@@ -19,35 +19,27 @@ void Memtrack::getNvmapMemory(int32_t pid, hidl_vec<MemtrackRecord> records) {
     struct nvmap_debugfs_handles_entry  entry;
 
     records.resize(8);
-    records[0].flags = MemtrackFlag::SMAPS_ACCOUNTED   | MemtrackFlag::PRIVATE
-                     | MemtrackFlag::DEDICATED | MemtrackFlag::NONSECURE;
-    records[1].flags = MemtrackFlag::SMAPS_UNACCOUNTED
-                     | MemtrackFlag::PRIVATE    | MemtrackFlag::DEDICATED | MemtrackFlag::NONSECURE;
-    records[2].flags = MemtrackFlag::SMAPS_ACCOUNTED
-                     | MemtrackFlag::SHARED_PSS | MemtrackFlag::DEDICATED | MemtrackFlag::NONSECURE;
-    records[3].flags = MemtrackFlag::SMAPS_UNACCOUNTED
-                     | MemtrackFlag::SHARED_PSS | MemtrackFlag::DEDICATED | MemtrackFlag::NONSECURE;
-    records[4].flags = MemtrackFlag::SMAPS_ACCOUNTED
-                     | MemtrackFlag::PRIVATE    | MemtrackFlag::DEDICATED | MemtrackFlag::SECURE;
-    records[5].flags = MemtrackFlag::SMAPS_UNACCOUNTED
-                     | MemtrackFlag::PRIVATE    | MemtrackFlag::DEDICATED | MemtrackFlag::SECURE;
-    records[6].flags = MemtrackFlag::SMAPS_ACCOUNTED
-                     | MemtrackFlag::SHARED_PSS | MemtrackFlag::DEDICATED | MemtrackFlag::SECURE;
-    records[7].flags = MemtrackFlag::SMAPS_UNACCOUNTED
-                     | MemtrackFlag::SHARED_PSS | MemtrackFlag::DEDICATED | MemtrackFlag::SECURE;
+    records[0].flags = MemtrackFlag::SMAPS_ACCOUNTED   | MemtrackFlag::PRIVATE    | MemtrackFlag::DEDICATED | MemtrackFlag::NONSECURE;
+    records[1].flags = MemtrackFlag::SMAPS_UNACCOUNTED | MemtrackFlag::PRIVATE    | MemtrackFlag::DEDICATED | MemtrackFlag::NONSECURE;
+    records[2].flags = MemtrackFlag::SMAPS_ACCOUNTED   | MemtrackFlag::SHARED_PSS | MemtrackFlag::DEDICATED | MemtrackFlag::NONSECURE;
+    records[3].flags = MemtrackFlag::SMAPS_UNACCOUNTED | MemtrackFlag::SHARED_PSS | MemtrackFlag::DEDICATED | MemtrackFlag::NONSECURE;
+    records[4].flags = MemtrackFlag::SMAPS_ACCOUNTED   | MemtrackFlag::PRIVATE    | MemtrackFlag::SYSTEM    | MemtrackFlag::NONSECURE;
+    records[5].flags = MemtrackFlag::SMAPS_UNACCOUNTED | MemtrackFlag::PRIVATE    | MemtrackFlag::SYSTEM    | MemtrackFlag::NONSECURE;
+    records[6].flags = MemtrackFlag::SMAPS_ACCOUNTED   | MemtrackFlag::SHARED_PSS | MemtrackFlag::SYSTEM    | MemtrackFlag::NONSECURE;
+    records[7].flags = MemtrackFlag::SMAPS_UNACCOUNTED | MemtrackFlag::SHARED_PSS | MemtrackFlag::SYSTEM    | MemtrackFlag::NONSECURE;
 
     std::ifstream ifnode(NVMAP_DEBUGFS_DIR + std::to_string(pid), std::ifstream::binary);
     if (ifnode.is_open()) {
         ifnode.read((char*)&hdr, sizeof(struct nvmap_debugfs_handles_header));
         if (ifnode && hdr.version == 1) {
             while (ifnode.read((char*)&entry, sizeof(struct nvmap_debugfs_handles_entry))) {
-                if ((entry.flags & NVMAP_HANDLE_SECURE) && (entry.share_count > 1)) {
+                if ((entry.base == 0) && (entry.share_count > 1)) {
                      records[6].sizeInBytes += (entry.mapped_size / entry.share_count);
                      records[7].sizeInBytes += ((entry.size - entry.mapped_size) / entry.share_count);
-                } else if ((entry.flags & NVMAP_HANDLE_SECURE) && (entry.share_count <= 1)) {
+                } else if ((entry.base == 0) && (entry.share_count <= 1)) {
                      records[4].sizeInBytes += entry.mapped_size;
                      records[5].sizeInBytes += (entry.size - entry.mapped_size);
-		} else if (!(entry.flags & NVMAP_HANDLE_SECURE) && (entry.share_count > 1)) {
+		} else if ((entry.base != 0) && (entry.share_count > 1)) {
                      records[2].sizeInBytes += (entry.mapped_size / entry.share_count);
                      records[3].sizeInBytes += ((entry.size - entry.mapped_size) / entry.share_count);
                 } else {
